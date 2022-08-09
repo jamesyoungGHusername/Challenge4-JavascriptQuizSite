327e30a163d0d6a3b5f0894cb6e5f7ac1ef5893f
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-08-09 16:26:52 -0600

    updated JS class structure to support the backend of the quiz. Can now create quiz and question objects and use those to build quiz display.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,49 @@ var questionText=document.querySelector(".questionText");
 var progressString=document.querySelector(".progressDisplay");
 var exampleButton=document.querySelector(".responseItem");
 
+//response option class that contains the text to be displayed, and whether or not this is a correct answer.
+class RespOption{
+    constructor(text,trueOrFalse){
+        this.text=text;
+        this.trueOrFalse=trueOrFalse;
+    }
+}
 
+//A question class that holds the text of the question, and an array of possible answers to be displayed.
+class MultipleChoiceQuestion{
+    constructor(text,answers =[]){
+        this.text=text;
+        this.answers=answers;
+    }
+    checkResponse(atIndex) {
+        return this.answers[atIndex].trueOrFalse;
+    }
+}
+
+//A quiz class that contains a title, an array of questions, as well as the methods and variables related to running the quiz.
+//As of now this doesn't make any allowances for the fact that javascript is not statically typed. TODO: add checks to ensure validity, or create a QuestionFactory to standardize input.
+class Quiz{
+    constructor(title,questions =[]){
+        this.title=title;
+        this.questions=questions;
+        this.currentQstIndex=0;
+        this.score=0;
+        this.quizActive=true;
+    }
+    getCurrentQuestion(){
+        return this.questions[this.currentQstIndex];
+    }
+    incrementQuestionIndex(){
+        if(this.currentQstIndex<this.questions.length-1 && this.quizActive){
+            this.currentQstIndex++;
+        }
+    }
+    decrementQuestionIndex(){
+        if(this.currentQstIndex<0 && this.quizActive){
+            this.currentQstIndex--;
+        }
+    }
+}
 
 
 function changeText(){
@@ -10,6 +52,9 @@ function changeText(){
 }
 
 function checkAnswer(){
+
+}
+function addButton(){
 
 }
 
